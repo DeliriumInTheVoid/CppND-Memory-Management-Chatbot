9eb45da658ded22622148785e5a66092ebe6b59b
ilia volkov <ivolkov@playtika.com>
2024-01-08 23:13:20 +0200

    Task 2 : The Rule Of Five In file chatbot.h / chatbot.cpp, make changes to the class ChatBot such that it complies with the Rule of Five

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -44,7 +44,44 @@ ChatBot::~ChatBot()
 
 //// STUDENT CODE
 ////
+ChatBot::ChatBot(ChatBot&& other)
+{
+    std::cout << "ChatBot Move Constructor" << std::endl;
+    _image = other._image;
+    other._image = nullptr;
 
+    _currentNode = other._currentNode;
+    other._currentNode = nullptr;
+
+    _rootNode = other._rootNode;
+    other._rootNode = nullptr;
+
+    _chatLogic = other._chatLogic;
+    other._chatLogic = nullptr;
+}
+
+ChatBot& ChatBot::operator=(ChatBot&& other)
+{
+    std::cout << "ChatBot Move Assignment Operator" << std::endl;
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    _image = other._image;
+    other._image = nullptr;
+
+    _currentNode = other._currentNode;
+    other._currentNode = nullptr;
+
+    _rootNode = other._rootNode;
+    other._rootNode = nullptr;
+
+    _chatLogic = other._chatLogic;
+    other._chatLogic = nullptr;
+
+    return *this;
+}
 ////
 //// EOF STUDENT CODE
 
